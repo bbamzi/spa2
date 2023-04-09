c61640250d47bae57e0a5b39cd16457fb389fd3e
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-04-09 21:40:27 +0100

    {-}

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import *
 
 # Create your views here.
 def index(request):
@@ -7,16 +8,19 @@ def index(request):
      return render(request, 'index.html',{"hello": "world"})
 
 def appointments(request):
-     
-     name = request.GET.get('name', 'N/A')
-     email=request.GET.get('email',"None")
-     date = request.GET.get('date', None)
-     time = request.GET.get('time', None)
-     service = request.GET.get('service', None)
-     payment_method = request.GET.get('payment_method', None)
-     service_type = request.GET.get('service_type', None)
-     durations = request.GET.get('durations', None)
-     receipt = request.GET.get('receipt', None)
+     if request.method == 'POST':
+          new_booking = Bookings(
+          name = request.GET.get('name', 'N/A'),
+          email=request.GET.get('email',"None"),
+          date = request.GET.get('date', None),
+          time = request.GET.get('time', None),
+          service = request.GET.get('service', None),
+          payment_method = request.GET.get('payment_method', None),
+          service_type = request.GET.get('service_type', None),
+          durations = request.GET.get('durations', None),
+          receipt = request.GET.get('receipt', None)
+          )
+          new_booking.save()
      
      return JsonResponse({"Wewe":"wewe"}, safe=False)
 
