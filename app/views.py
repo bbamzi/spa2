749b0918efddac3533a5ae41c81141a7bd01603b
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-04-09 18:48:56 +0100

    {+}

--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
 def index(request):
@@ -6,8 +7,19 @@ def index(request):
      return render(request, 'index.html',{"hello": "world"})
 
 def appointments(request):
+     
+     name = request.GET.get('name', 'N/A')
+     email=request.GET.get('email',"None")
+     date = request.GET.get('date', None)
+     time = request.GET.get('time', None)
+     service = request.GET.get('service', None)
+     payment_method = request.GET.get('payment_method', None)
+     service_type = request.GET.get('service_type', None)
+     durations = request.GET.get('durations', None)
+     resp = {"success": "sdsdsd"}
+     return JsonResponse(resp, safe=False)
 
-     return render(request, 'appointment.html',{"hello": "world"})
+     
 
 
 def opening(request):
