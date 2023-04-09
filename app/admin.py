ba4b129dc10d4ca1e7198f971e86a19cbb3cb699
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-04-10 00:30:44 +0100

    {+}

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import *
+
+class paymentAdmin(admin.ModelAdmin):
+    model= PaymentDetail
+
+class bookingAdmin(admin.ModelAdmin):
+    model= Booking
+   
+
+
 # Register your models here.
+admin.site.register(PaymentDetail, paymentAdmin)
+admin.site.register(Booking, bookingAdmin)
--- app/models.py
+++ app/models.py
@@ -10,8 +10,9 @@ ID_LENGTH = 9
 def id_gen() -> str:
     """Generates random string whose length is `ID_LENGTH`"""
     return int_to_base36(uuid.uuid4().int)[:ID_LENGTH]
-class Bookings(models.Model):
-    order_id = models.CharField(max_length=ID_LENGTH, primary_key=True, default=id_gen, editable=False)
+
+class Booking(models.Model):
+    order_id = models.CharField(max_length=ID_LENGTH, unique = True , default=id_gen, editable=False)
     order_date = models.DateField(auto_now_add=True)
     order_time =models.TimeField(auto_now=False, auto_now_add=False)
     name =  models.CharField(max_length=100, blank=True, null=True)
@@ -28,14 +29,14 @@ class Bookings(models.Model):
 
 
     def __str__(self) -> str:
-        return f'order id: {self.id}'
+        return f'order--{self.order_id} created {self.order_date}'
     
     
-    class PaymentDetails(models.Model):
-        cashApp =  models.CharField(max_length=100, blank=True, null=True)
-        venmo =  models.CharField(max_length=100, blank=True, null=True)
-        paypal =  models.CharField(max_length=100, blank=True, null=True)
-        bitcoin =  models.CharField(max_length=100, blank=True, null=True)
-        zelle =  models.CharField(max_length=100, blank=True, null=True)
+class PaymentDetail(models.Model):
+    cashApp =  models.CharField(max_length=100, blank=True, null=True)
+    venmo =  models.CharField(max_length=100, blank=True, null=True)
+    paypal =  models.CharField(max_length=100, blank=True, null=True)
+    bitcoin =  models.CharField(max_length=100, blank=True, null=True)
+    zelle =  models.CharField(max_length=100, blank=True, null=True)
         
 
