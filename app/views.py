681626dcd1a05248f84bf685b833e0ca24e15abe
Bamigboye Akinbode <bodejuniorforever@gmail.com>
2023-04-13 14:17:45 +0100

    {+}

--- app/views.py
+++ app/views.py
@@ -8,6 +8,24 @@ from django.core.files.storage import FileSystemStorage
 from django.core.files.base import ContentFile
 import base64
 import os
+from django.template.loader import render_to_string
+import smtplib
+from email.message import EmailMessage
+
+
+
+def send_email(message,email):
+
+  smtp = smtplib.SMTP_SSL('smtp.gmail.com',465)
+  smtp.login( os.getenv("sender"), os.getenv("password"))
+  msg = EmailMessage()
+  msg['Subject'] ='Your Invoice'
+  msg['From'] = 'Invoice'
+  msg['To']=email
+  msg.set_content('Your Invoice')
+  msg.add_alternative(message,subtype='html')
+  smtp.send_message(msg)
+  print('sent')
 
 def id_gen() -> str:
     """Generates random string whose length is `ID_LENGTH`"""
@@ -39,8 +57,28 @@ def appointments(request):
           )
           new_booking.save()
           details = PaymentDetail.objects.get(payment_type = request.POST.get('payment_method', None))
+          # order_details = Booking.objects.get(order_id =gen_id )
           resp = {"order_id":gen_id,"payment_details":details.payment_value}
+          html_message = render_to_string(
+            "email_receipt.html",
+            context={"order_id":gen_id,
+                     "today":datetime.today().date,
+                     "payment":request.POST.get('payment_method', None),
+                     "service_type":request.POST.get('service_type', None),
+                     "email":request.POST.get('email',"None"),
+                     "order_status":"Awaiting_payment",
+                     "name":request.POST.get('name'),
+                     "client_address":request.POST.get('address', None),
+                     "service":request.POST.get('service', None),
+                     "total":request.POST.get('total', None),
+                     "details":details.payment_value
+                     },
+        )
+          
+          send_email(html_message,request.POST.get('email',"None"))
           return JsonResponse(resp)
+         
+
      
      return render(request, 'appointment.html',{"hello": "world", "order_id":gen_id})
 
@@ -58,7 +96,7 @@ def updateReceipt(request):
           filename = fss.save(f'{id}.{extension}', file)
           url = fss.url(filename)
           order.receipt = url
-          order.save()
+          # order.save()
                     
 
           return JsonResponse({"status":"success"})
